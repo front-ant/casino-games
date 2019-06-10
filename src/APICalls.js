c9e334c18269b7ab38047b99799639da940bdf15
anttheant <ant@capybara.eu>
2019-06-10 21:59:36 +0100

    Set up fake server

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,3 +1,3 @@
-export const loadGames = () => fetch("../src/gameData.json").then(res => res.json());
+export const loadGames = () => fetch("../src/data/gameData.json").then(res => res.json());
 
-export const validateLogin = () => fetch("../src/LoginRequest.json").then(res => res.json());
+// export const validateLogin = () => fetch("../src/LoginRequest.json").then(res => res.json());
