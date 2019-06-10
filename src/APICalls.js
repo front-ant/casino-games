872134b6d3529c5b1fd389aed7cb11d96de7120f
anttheant <ant@capybara.eu>
2019-06-10 23:12:29 +0100

    Create clean reop

---
+++ src/APICalls.js
@@ -0,0 +1,12 @@
+export const loadGames = () => fetch("../src/data/gameData.json").then(res => res.json());
+
+export const checkLogin = (value) =>
+ fetch("http://localhost:3000/users", {
+  headers: {"Content-Type": "application/json; charset=utf-8"},
+  method: "POST",
+    body: JSON.stringify({
+    username: value
+  })
+})
+  .then(fetch(`http://localhost:3000/users?username=${value}`))
+  .then(res => res.json());
