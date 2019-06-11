30ea9f069c4c1c66fc8ad27db85f23ec941c77c8
anttheant <ant@capybara.eu>
2019-06-11 13:50:04 +0100

    Clean up

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,12 +1,13 @@
 export const loadGames = () => fetch("../src/data/gameData.json").then(res => res.json());
 
 export const checkLogin = (value) =>
- fetch("http://localhost:3000/users", {
-  headers: {"Content-Type": "application/json; charset=utf-8"},
-  method: "POST",
+  fetch("http://localhost:3000/users", {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    method: "POST",
     body: JSON.stringify({
-    username: value
+      username: value
+    })
   })
-})
-  .then(fetch(`http://localhost:3000/users?username=${value}`))
   .then(res => res.json());
